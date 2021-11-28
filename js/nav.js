0f5a7f67f7ff44282e84e5d8c9c1e2eb83e61c57
aricodeine <88302441+aricodeine@users.noreply.github.com>
2021-11-29 01:25:50 +0530

    Removing unnecessary code from nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,12 @@
 const navSlide =() =>{
     const burger = document.querySelector('.burger');
     const nav= document.querySelector('.nav-links');
-    const cal= document.querySelector('.calendar');
+//     const cal= document.querySelector('.calendar');
     const navLinks = document.querySelectorAll('.nav-links li');
     //Toggle Nav
     burger.addEventListener('click', ()=>{
         nav.classList.toggle('nav-active');
-        cal.classList.toggle('cal-hide');
+//         cal.classList.toggle('cal-hide');
     });
 
     //links
